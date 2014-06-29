16a7cf4706b54d670d681d6a417f468f6f5823a7
Michael Sanchez <michael.h.sanchez@gmail.com>
2014-06-29 08:18:05 -0400

    associate listings with users

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -10,6 +10,8 @@ has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }
  validates :name, :description, :price, presence: true 
  validates :price, numericality: { greater_than: 0 } 	
  validates_attachment_presence :image
+
+ belongs_to :user
  validates_attachment_content_type :image, :content_type => %w(image/jpeg image/jpg image/png)
 
 end
