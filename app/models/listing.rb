fbf1c184534169aa1177a017534ece9956a4a5ae
Michael Sanchez <michael.h.sanchez@gmail.com>
2014-06-29 21:32:25 -0400

    enable users to place orders

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -12,6 +12,7 @@ has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }
  validates_attachment_presence :image
 
  belongs_to :user
+ has_many :orders	
  validates_attachment_content_type :image, :content_type => %w(image/jpeg image/jpg image/png)
 
 end
